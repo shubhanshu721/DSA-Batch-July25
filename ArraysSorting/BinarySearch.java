08012e0c6eeb0bbe9d47847923e23e6bc17f23b2
Shubhanshu Shukla <shubhanshushukla@Shubhanshus-MacBook-Air.local>
2025-08-10 10:17:46 +0530

    Arrays sorting binary search

---
+++ ArraysSorting/BinarySearch.java
@@ -0,0 +1,13 @@
+package ArraysSorting;
+
+//Binary Search is a search algorithm that finds the position of a target value within a sorted array.
+//We need to opt for divide and conquer approach to implement binary search.
+//First take out the middle element of the array and compare it with the target value.
+
+public class BinarySearch {
+
+    public static void main (String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int mid =
+    }
+}
